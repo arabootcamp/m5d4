4895db8c5308927b7c549ce4e2ea920a8e33aca2
aracelly <aracellybootcamp@gmail.com>
2021-10-18 19:59:55 -0300

    desafio 4

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6" md="4" v-for="el in personajes" :key="el.id">
+          <v-card elevation="2" max-width="300" class="mx-auto">
+            <v-img height="300" :src="el.imagen" :alt="el.nombre"></v-img>
+            <v-card-title>{{el.nombre}}</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  import {
+    defineComponent
+  } from '@vue/composition-api'
+
+  export default {
+    name: 'index',
+    computed: {
+      personajes() {
+        return this.$store.getters['getPersonajes'];
+      }
+    }
+  }
+
+</script>
